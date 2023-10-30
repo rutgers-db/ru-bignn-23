b44eb880ef56f4fd60edc18b50e22e08304b3078
Ubuntu <ubuntu@ip-172-31-86-248.ec2.internal>
2023-10-30 07:12:24 +0000

    update

--- contest-scripts/docker_run_container.sh
+++
@@ -1,3 +0,0 @@
-CONTEST_REPO_PATH=/home/ubuntu/big-ann-benchmarks #path to big-ann-benchmarks
-INDEX_FILE_PATH=~/index_file
-docker container run -it  --mount type=bind,src=$CONTEST_REPO_PATH/results,dst=/home/app/results type=bind,src=$INDEX_FILE_PATH,dst=/home/app/index_file --read-only --mount type=bind,src=$CONTEST_REPO_PATH/data,dst=/home/app/data  neurips23-filter-rubignn  /bin/bash
